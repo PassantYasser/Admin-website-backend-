23aa79f21e5c7840533a40360784a36880c3a577
PassantYasser <114351321+PassantYasser@users.noreply.github.com>
2025-01-13 16:26:40 +0200

    UI

--- app.js
+++ app.js
@@ -1,50 +1,34 @@
 const express = require('express')
 const app = express()
-const port = 3000
-//mongodb
-const mongoose = require('mongoose');
-//to send data
-app.use(express.urlencoded({ extended: true }));
-//ejs
-app.set('view engine', 'ejs')
-//static files (css html js files)
-app.use(express.static('Public'))
-
-// app.get('/', (req, res) => {
-//   res.sendFile('./Views/Home.ejs',{root:__dirname})
-// })
+const port = 3000;
+const mongoose = require('mongoose'); //mongodb
+app.use(express.urlencoded({ extended: true })); //to send data
+app.set('view engine', 'ejs'); //ejs
+app.use(express.static('Public')); //static files (css html js files)
 
 
-//post required 
-const modelDataPerson = require("./Model/personTable")
-app.post ("/",(req , res)=>{
-  const x = new modelDataPerson(req.body)
-  x.save() 
-  .then(() => {
-    res.redirect("/");
-  })
-  .catch( err => {
-    console.log(err);
-  });
-})
 
-// read data
 
 app.get('/', (req, res) => {
-  modelDataPerson.find()
-    .then((result) => { 
-      console.log(result);
-        res.render('Home' , {title:'heloo',arr:result})
-    })
-    .catch((err) => { 
-      console.log(err)
-    })
+  res.render('index' , { })
   })
+  
+app.get('/user/add.html', (req, res) => {
+  res.render('user/add' , { })
+  })
+app.get('/user/edit.html', (req, res) => {
+  res.render('user/edit' , { })
+  })
+app.get('/user/view.html', (req, res) => {
+  res.render('user/view' , { })
+  })
+      
+    
+  
   
 
 
-
-//connect mongoDB
+//connection of mongoDB
 mongoose
 .connect("mongodb+srv://passantabutalib:pwPwdohiI12ZQ8GA@cluster0.odavh.mongodb.net/MyData?retryWrites=true&w=majority&appName=Cluster0") 
 .then(()=>{
